596c6299b21950adce3c5115a87cef5edb86eea0
Apollo_Dot <72624705+Apollo-Dot@users.noreply.github.com>
2023-01-02 16:21:46 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [preact()],
+    }
+  } else {
+    return {
+      plugins: [preact()],
+      base: '/personal-website-v3/'
+    }
+  }
 })
