8fe7c0f3f377547e01347173cbfa07d23757770c
Apollo_Dot <72624705+Apollo-Dot@users.noreply.github.com>
2023-01-03 07:36:27 -0500

    v4.2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { BsTwitch, BsGithub, BsYoutube } from 'react-icons/bs'
 export function NavBar() {
     return (
         <div id='navBar' className='bg-red-700 w-screen h-12 border-b-4 border-red-900 text-white relative'>
-            <div id='links' className='p-2 absolute top-0 left-0 flex gap-2'>
+            <div id='links' className='hidden p-2 absolute top-0 left-0 sm:flex gap-2'>
                 <a href='https://github.com/Apollo-Dot' target='_blank'><BsGithub size='25' /></a>
                 <a href='https://www.twitch.tv/apollo_dot' target='_blank'><BsTwitch size='25' /></a>
                 <a href='https://www.youtube.com/@apollo_dot4586' target='_blank'><BsYoutube size='25' /></a>
