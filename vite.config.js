fad5c381e810f177b1dac6a3800593f08b601250
Apollo-Dot <72624705+Apollo-Dot@users.noreply.github.com>
2023-01-21 08:54:22 -0500

    remaking with vue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import preact from '@preact/preset-vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    preact(),
-    VitePWA({ registerType: 'autoUpdate' })
-  ],
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
